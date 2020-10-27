5b47561b3af98243adfea21270003d7351766c4d
Chris Park <chris.park@aligent.com.au>
2020-10-27 20:10:32 +1030

    multi-service cluster with multi-container task

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs'
 import * as ecr from '@aws-cdk/aws-ecr'
+import { countReset } from 'console';
 
 
 export class EcsStack extends cdk.Stack {
@@ -13,49 +14,62 @@ export class EcsStack extends cdk.Stack {
         const pjt_name = this.node.tryGetContext('project_name');
         const env_name = this.node.tryGetContext('env');
         
-        const ecr_path = '528928441350.dkr.ecr.ap-southeast-2.amazonaws.com/'
-
-        // Create ECR 
-        const reponames = ['code', 'app', 'cron', 'management']
-        for (var repo of reponames) {
-            const repository = new ecr.Repository(this, repo+'-repo', {
-                imageScanOnPush: true,
-                repositoryName: repo
-                }
-            )
-        }
+        // const ecr_path = '528928441350.dkr.ecr.ap-southeast-2.amazonaws.com/'
         
-        const repo_test = new ecr.Repository(this, 'test-repo')
-
-        
-        // Create ECS Cluster
-        const cluster = new ecs.Cluster(this, 'Backend-cluster', {
+        // Creation order in a cluster is important due to the referencing order. 
+        // Cluster && TaskDef before ContainerDef (which TaskDef this belongs to) before Service (which TaskDef - must have one or more essential Container - to use)
+        // Create ECS Clusters
+        const backendCluster = new ecs.Cluster(this, 'Backend-cluster', {
             capacity: {
                 instanceType: new ec2.InstanceType('t2.micro'), // Needs to be parameterised via env setup in app.ts
                 allowAllOutbound: true //Default. Container instances do require external network access to communicate with the Amazon ECS service endpoint. Complete the following steps to add these optional security group rules.
             },
             vpc: vpc
         })
+        // Task Definition. 
+        const appTaskDefinition = new ecs.Ec2TaskDefinition(this, 'App-taskdefinition')
+        const utilTaskDefinition = new ecs.Ec2TaskDefinition(this, 'Util-taskdefinition')
 
-        // Task Definition
-        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'Backend-taskdefinition')
 
         // ECS Container Definition
         // To-do: control outbound allowlist
-        const containerCron = new ecs.ContainerDefinition(this, 'cron', {
-            image: ecs.ContainerImage.fromEcrRepository(repo_test, '0001'), // To-do: itertate or reference the repositories created in the for..loop.
-            taskDefinition: taskDefinition,
+        const appImageTag = '0001' // Needs to be parameterised via env setup in app.ts
+        const appRepo = new ecr.Repository(this, 'app-repo', {
+            imageScanOnPush: true,
+            repositoryName: 'app',
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        })
+        const appContainer = new ecs.ContainerDefinition(this, 'app', {
+            image: ecs.ContainerImage.fromEcrRepository(appRepo, appImageTag), 
+            taskDefinition: appTaskDefinition,
+            memoryLimitMiB: 128,
+            essential: true
+        })
+        const appService = new ecs.Ec2Service(this, 'App-service', {
+            cluster: backendCluster,
+            taskDefinition: appTaskDefinition
+        })
+
+        const cronImageTag = '0001' // Needs to be parameterised via env setup in app.ts
+        const cronRepo = new ecr.Repository(this, 'cron-repo', {
+            imageScanOnPush: true,
+            repositoryName: 'cron',
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        })
+        const cronContainer = new ecs.ContainerDefinition(this, 'cron', {
+            image: ecs.ContainerImage.fromEcrRepository(cronRepo, cronImageTag), 
+            taskDefinition: utilTaskDefinition,
+            memoryLimitMiB: 64,
+            essential: true
+        })
+        const utilAppContainer = new ecs.ContainerDefinition(this, 'util-app', {
+            image: ecs.ContainerImage.fromEcrRepository(appRepo, appImageTag), 
+            taskDefinition: utilTaskDefinition,
             memoryLimitMiB: 64
         })
-
-        // ECS EC2Service (vs Fargate)
-        const service = new ecs.Ec2Service(this, 'Backend-service', {
-            cluster: cluster,
-            taskDefinition: taskDefinition
+        const utilService = new ecs.Ec2Service(this, 'Util-service', {
+            cluster: backendCluster,
+            taskDefinition: utilTaskDefinition
         })
-
-
-
-        // Create EC2 ASG for ECS Cluster
     }
 }
